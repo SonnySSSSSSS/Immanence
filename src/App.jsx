5878680645ff525663e7a600a1752d0e4486d09d
SonnySSSSSSS <trinh.sonny@gmail.com>
2026-01-27 13:52:26 +0700

    bump version to v3.25.55

--- src/App.jsx
+++ src/App.jsx
@@ -381,7 +381,7 @@ function App() {
                       className={`text-[8px] uppercase tracking-[0.15em] ${isLight ? 'text-[#5A4D3C]/50' : 'text-white/40'}`}
                       style={{ fontFamily: 'var(--font-display)' }}
                     >
-                        v3.25.54
+                        v3.25.55
                     </div>
                   </div>
 
