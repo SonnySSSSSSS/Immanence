a7c8bec7cd9e790be7e55085a618967da711cdcf
SonnySSSSSSS <trinh.sonny@gmail.com>
2026-02-09 20:34:31 +0700

    chore: bump version to v3.27.164

--- src/App.jsx
+++ src/App.jsx
@@ -542,7 +542,7 @@ function App({ playgroundMode = false, playgroundBottomLayer = true }) {
                         }}
                         style={{ background: 'transparent' }}
                       >
-                        v3.27.163
+                        v3.27.164
                       </button>
                     </div>
                   </div>
